1bee324e360e2afdca5b903067f7e09350843202
Askhab <asmag86@gmail.com>
2021-02-02 16:01:34 +0300

    First commit

---
+++ js/sctipt.js
@@ -0,0 +1,9 @@
+"use strict";
+
+window.addEventListener("DOMCOntentLoaded", () => {
+    const tabs = document.querySelectorAll(".tabheader__item"),
+          tabsContent = document.querySelectorAll(".tabcontent"),
+          tabsParent = document.querySelector(".tabheader__items");
+
+    
+});
