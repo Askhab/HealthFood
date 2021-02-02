a5b65f970982da683e5860676d8dff89a8eedc21
Askhab <asmag86@gmail.com>
2021-02-02 17:32:11 +0300

    First logic with tabheaders

--- js/sctipt.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-window.addEventListener("DOMCOntentLoaded", () => {
-    const tabs = document.querySelectorAll(".tabheader__item"),
-          tabsContent = document.querySelectorAll(".tabcontent"),
-          tabsParent = document.querySelector(".tabheader__items");
-
-    
-});
