3d4a5c795eaa6d87065e6ab822d3b236d9d0d22e
Askhab Magomedov <asmag86@gmail.com>
2021-02-07 01:33:10 +0300

    Added server.php file for work with POST methods, added some logis on JS for forms

--- js/script.js
+++ js/script.js
@@ -129,7 +129,7 @@ window.addEventListener("DOMContentLoaded", () => {
         }
     });
 
-    // const modalTimerId = setTimeout(openModal, 5000);
+    const modalTimerId = setTimeout(openModal, 300000);
 
     function showModalByScroll() {
         if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
@@ -214,5 +214,60 @@ window.addEventListener("DOMContentLoaded", () => {
         '.menu .container',
         'menu__item'
     ).render();
+
+    // Forms
+
+    const forms = document.querySelectorAll("form");
+
+    const message = {
+        loading: "Loading...",
+        success: "Thanks! Call you later!",
+        failure: "Something gone wrong.",
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener("submit", (e) => {
+            e.preventDefault();
+
+            let statusMessage = document.createElement("div");
+            statusMessage.classList.add("status");
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open("POST", "server.php");
+
+            // Если используешь связку XMLHttpRequest & FormData заголовок ненужен иначе ошибка - данные не дойдут до сервера
+            // request.setRequestHeader("Content-type", "multipart/form-data");
+            request.setRequestHeader("Content-type", "application/json");
+            const formData = new FormData(form);
+
+            const object = {};
+            formData.forEach(function(value, key) {
+                object[key] = value;
+            });
+
+            const json = JSON.stringify(object);
+
+            request.send(json);
+
+            request.addEventListener("load", () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000);
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+        });
+    }
 });
 
