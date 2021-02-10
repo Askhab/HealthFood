12bc6fc52802e88d73b8cf22290272992a94ce30
Askhab <asmag86@gmail.com>
2021-02-10 15:05:36 +0300

    Added JSON-server but it's not working now, Have to try it in home PC

--- js/script.js
+++ js/script.js
@@ -296,4 +296,8 @@ window.addEventListener("DOMContentLoaded", () => {
         }, 4000);
     }
 
+    fetch("db.json")
+        .then(data => data.json())
+        .then(res => console.log(res));
+
 });
